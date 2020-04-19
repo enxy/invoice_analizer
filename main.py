739ae7d159355c28dbc2206f54f7b0f5faa2ad3c
Jolanta Plewa <jol.plewa@gmail.com>
2020-04-19 16:09:15 +0200

    Added invoice_analizer/main.py script

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import pyspark
 from invoiceExtractor import extract_data
+from invoiceExtractor.main import analize_data
 from invoiceExtractor.extract.loader import read_templates
+import re, os
 
-templates = read_templates('tpl')
-result = extract_data('../invoices/inv2.pdf', templates=templates)
+CURRENT_DIR = os.path.dirname(__file__)
+TPL_DIR = os.path.join(CURRENT_DIR, 'tpl')
+INVOICES_DIR = os.path.join(CURRENT_DIR, 'invoices')
+TEMPLATES = read_templates(TPL_DIR)
 
-print(result)
+ANALIZED_DATA = []
+
+for invoice in os.listdir(INVOICES_DIR):
+    result = extract_data(os.path.join(INVOICES_DIR, invoice), templates=TEMPLATES)
+    ANALIZED_DATA.append(analize_data(result))
+
+print(ANALIZED_DATA)
